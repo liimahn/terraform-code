ddc01412e4ea1c7bae88b770e3a2c369a14447b5
liimahn <layiwolalag2010@yahoo.co.uk>
2024-05-24 03:23:26 +0100

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+my_region     = "us-east-1"
+profile       = "default"
+topic_name    = "sns-dev"
+endpoint      = "lag.ola@yahoo.com"
+protocol_type = "email"
+team          = "devops"
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+}
+variable "team" {
+  default = "devops"
+}
+variable "protocol_type" {
+  default = "email"
+}
+variable "endpoint" {
+  default = "lag.ola@yahoo.com"
+}
